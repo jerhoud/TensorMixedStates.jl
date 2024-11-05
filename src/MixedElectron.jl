13cea618c011d2bbd1a0bcbac0842b919080722f
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2024-11-05 22:46:10 +0100

    Added new kinds of sites

---
+++ src/MixedElectron.jl
@@ -0,0 +1,12 @@
+@mixer(
+    "Electron",
+    false,
+    [
+        "Emp", "Up", "Dn", "UpDn", "0", "↑", "↓", "↑↓",
+    ],
+    [
+        "Nup", "Ndn", "Nupdn", "Ntot",
+        "Aup", "Adagup", "Adn", "Adagdn",
+    ],
+    []
+)
