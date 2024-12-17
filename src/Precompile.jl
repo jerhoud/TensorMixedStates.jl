3128584e73d5b0147572108fdabfc609cc01bea5
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2024-12-17 15:39:06 +0100

    improved Precompilation.jl

--- src/Precompile.jl
+++ src/Precompile.jl
@@ -3,10 +3,13 @@ using PrecompileTools: @compile_workload
 @compile_workload begin
     s = System(3, "Qubit")
     stp = State(Pure, s, "+")
-    stm = State(Mixed, s, "Y+")
+    stm = State(Mixed, s, ["Half", "Y+", "Z+"])
     sstp = string(stp)
     sstm = string(stm)
-    m = Measure(X, Y(1), Z(2)Y(1) + Z(3)Y(2), (X, Y))
+    m = Measure(X, Y(1), Z(2)Y(1) + Z(3)Y(2), (X, Y), Y, (Z, Z))
+    sm = string(m)
     mp = measure(stp, m)
     mm = measure(stm, m)
+    tdvp(-im*(Y(2)+2Z(1)X(3)), 0.1, stp; maxdim = 3)
+    approx_W(-im*(Y(2)+2Z(1)X(3), 0.1, stp; maxdim = 3)
 end
