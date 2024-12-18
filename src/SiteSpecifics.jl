744c87089e14cf0c22ab25336ad4681ea58643cb
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2024-12-18 12:06:08 +0100

    Improved graph_state

--- src/SiteSpecifics.jl
+++ src/SiteSpecifics.jl
@@ -3,11 +3,11 @@ export graph_state
 
 state(::StateName"Half", ::SiteType"MixedQubit") = [0.5 0 0 0.5]
 
-function graph_state(tp::Union{TPure, TMixed}, g::Vector{Tuple{Int, Int}}; kwargs...)
+function graph_state(tp::Union{TPure, TMixed}, g::Vector{Tuple{Int, Int}}; cutoff=1e-16, kwargs...)
     n = graph_base_size(g)
     s = System(n, "Qubit")
     state = State(tp, s, "+")
     gates = prod(CZ(i, j) for (i, j) in g)
-    state = apply(gates, state)
+    state = apply(gates, state; cutoff, kwargs...)
     return state
 end
