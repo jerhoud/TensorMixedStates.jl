4bb08f02f0122604b869385259165209ad60ab00
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2025-01-08 14:26:22 +0100

    Corrected bug preventing execution

--- src/PhaseTypes.jl
+++ src/PhaseTypes.jl
@@ -16,18 +16,6 @@ const no_limits = Limits(cutoff = 0, maxdim = typemax(Int))
 
 
 """
-    Phases = Union{CreateState, SaveState, LoadState, ToMixed, Evolve, Gates, Dmrg}
-
-A type that contains all possible phase types for SimData and runTMS.
-Each of the types contains at least the three following fields (like SimData).
-
-- `name`: the name of the phase
-- `time_start`: the simulation time to use at the start of the phase
-- `final_measures`: the measurements to make at the end of the phase see `Measure` and `output`
-"""
-Phases = Union{CreateState, SaveState, LoadState, ToMixed, Evolve, Gates, Dmrg}
-
-"""
 A phase type to create the simulation step
 
 # Fields
@@ -175,3 +163,15 @@ A phase type for optimizing with Dmrg
     measures_period::Int = 1
     tolerance::Number = 0.
 end
+
+"""
+    Phases = Union{CreateState, SaveState, LoadState, ToMixed, Evolve, Gates, Dmrg}
+
+A type that contains all possible phase types for SimData and runTMS.
+Each of the types contains at least the three following fields (like SimData).
+
+- `name`: the name of the phase
+- `time_start`: the simulation time to use at the start of the phase
+- `final_measures`: the measurements to make at the end of the phase see `Measure` and `output`
+"""
+Phases = Union{CreateState, SaveState, LoadState, ToMixed, Evolve, Gates, Dmrg}
