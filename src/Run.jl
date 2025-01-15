27712077202e66773fd6d9364775c3f06846e18d
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2025-01-15 17:41:23 +0100

    Corrected typo in doc

--- src/Run.jl
+++ src/Run.jl
@@ -11,7 +11,7 @@ A type for describing a simulation to use with `runTMS`
 - `phases`:          the list of phases of the simulation (see Phases for a list of possible values)
 - `descritpion`:     text put in the description file of the simulation (default "")
 - `time_start`:      initial simulation time (default 0.)
-- `final_measures`:  measures to make at the end of simulation (default []) see `Measure` and `output`
+- `final_measures`:  measures to make at the end of simulation (default []) see `measure` and `output`
 - `time_format`:     C like format for output of simulation time (default "%8.4g")
 - `data_format`:     C like format for output of simulation data (default "%12.6g")
 """
