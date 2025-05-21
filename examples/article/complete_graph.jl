cdd00787a790a17cefd375a7e5ec5185220d7a24
Jérôme Houdayer <jerome.houdayer@polytechnique.org>
2025-05-21 17:13:33 +0200

    Added the complete graph example

---
+++ examples/article/complete_graph.jl
@@ -0,0 +1,24 @@
+using TensorMixedStates, .Qubits
+
+dissipators(n, c) = c * sum(Dissipator(Sp)(i) for i in 1:n)
+
+simdata(n, c) = SimData(
+  name = "graph_$(n)_$c",
+  phases = [
+    create_graph_state(Pure(), complete_graph(n); limits = Limits(cutoff=1e-14)),
+    ToMixed(),
+    Evolve(
+      duration = 5,
+      time_step = 0.2,
+      algo = ApproxW(order=1, w=2),
+      limits = Limits(maxdim=20, cutoff=1e-20),
+      evolver = dissipators(n, c),
+      measures = [
+        "sanity.dat" => [Trace, Purity, Linkdim],
+        "data.dat" => Y(1)Y(2)Z(3)
+      ]
+    )
+  ]
+)
+
+runTMS(simdata(512, 1))
